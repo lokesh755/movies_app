0d7f3bd39787668387a06d4af769b84933facf01
Lokesh <lokesh755@github.com>
2020-06-16 10:08:46 -0400

    Added index page and new movie page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,23 @@
 class MoviesController < ApplicationController
-
     def show
-        @movies = ["Parasite", "Titanic"]
-        @movieDirectors = ["Bong Joon-Ho", "James Cameron"]
-        @movieId = params[:id].to_i
-        @movieName = @movies[@movieId - 1]
-        @movieDirector = @movieDirectors[@movieId - 1]
-    end 
-end
+      movie = Movie.find(params[:id])
+  
+      render :show, locals: { movie: movie }
+    end
+
+    def index
+        movies = Movie.all 
+        render :index, locals: { movies: movies }
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        attributes = { title: params[:movie][:title], director: params[:movie][:director] }
+        movie = Movie.create(attributes)
+        render :success
+    end
+
+  end
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -1,17 +1,41 @@
 require "application_system_test_case"
 
-class MoviesSystemTest < ApplicationSystemTestCase 
-    test "visiting the page" do 
-        visit 'movies/1'
-        assert_text "Parasite"
-        assert_text "Bong Joon-Ho"
-    end
+class MoviesSystemTest < ApplicationSystemTestCase
+  test "visiting the show page" do
+    attributes = { title: "Parasite", director: "Bong Joon-ho" }
+    movie = Movie.create(attributes)
 
-    test "visiting the show page for another movie" do
-        visit '/movies/2'
-    
-        assert_text "Titanic"
-        assert_text "James Cameron"
-      end
+    # As a user,
+    # When I visit /movies/1
+    visit "/movies/#{movie.id}"
+
+    # I see the title of the movie "Parasite"
+    assert_text "Parasite"
+    # I also see the name of the director "Bong Joon-ho"
+    assert_text "Bong Joon-ho"
+  end
+
+  test "visiting the show page for another movie" do
+    attributes = { title: "Titanic", director: "James Cameron" }
+    movie = Movie.create(attributes)
+
+    visit "/movies/#{movie.id}"
+
+    assert_text "Titanic"
+    assert_text "James Cameron"
+  end
+
+  test "visiting the index page" do
+    attributes = { title: "Titanic", director: "James Cameron" }
+    movie = Movie.create(attributes)
+
+    attributes = { title: "Parasite", director: "Bong Joon-ho" }
+    movie = Movie.create(attributes)
+
+    visit "/movies/"
+
+    assert_text "Titanic"
+    assert_text "Parasite"
+  end
+
 end
-
